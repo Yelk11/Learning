18ed81fd45617e0a67b9b46a62e396a388c8d380
Yelk11 <neighbour.matthew@gmail.com>
2021-12-09 13:42:28 -0500

    update

---
+++ DataStructures/linked_list/c/src/include/linked_list.h
@@ -0,0 +1,10 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+
+typedef struct NODE{
+    node_T* child;
+    int value;
+}node_T;
+
+#endif
---
+++ DataStructures/linked_list/c/src/linked_list.c
@@ -0,0 +1,28 @@
+#include "include/linked_list.h"
+
+
+
+
+
+int main(int argc, char* argv[]){
+    
+    node_T* head;
+    node_T* child_1;
+    node_T* child_2;
+
+    head->value = 9;
+    child_1->value = 1;
+    child_2->value = 2;
+
+    head->child = child_1;
+    child_1->child = child_2;
+
+    
+}
+
+
+
+
+
+
+
